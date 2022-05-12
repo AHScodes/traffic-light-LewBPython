934cb3c9ee7070d1bd47ed6b0177849b8e5f5b94
LewBPython <97629557+LewBPython@users.noreply.github.com>
2022-05-12 13:46:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 #Here
+
+import pyfirmata
+import time
+import threading
+
+
+#board = pyfirmata.Arduino("/dev/cu.usbmodem14101")
+#it = pyfirmata.util.Iterator(board)
+#it.start()
+
+
+class TL:
+
+    def __init__(self, color):
+        self.color = color
+        #state = 'green'
+    lleee = 1
+
+    def changeLight(self, lleee):
+        if lleee == 1:
+           print("lam")
+
+
+    #def carAppr(self):
+
+   # def
