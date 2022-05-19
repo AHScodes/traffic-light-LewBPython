3a3b2ef95ffa28a6e52b29e14192337556a183a1
LewBPython <97629557+LewBPython@users.noreply.github.com>
2022-05-19 10:15:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,17 +11,44 @@ import threading
 
 
 class TL:
-
     def __init__(self, color):
-        self.color = color
-        #state = 'green'
-    lleee = 1
+        self.color = "green" #defaut
+        self.carApr = False
+        self.runn = True
 
-    def changeLight(self, lleee):
-        if lleee == 1:
-           print("lam")
+    def printLight(self):
+        if TL.color == "green":
+           print("green, GO!")
+        if TL.color == "yellow":
+           print("green, SLOW!")
+        if TL.color == "red":
+           print("green, STOP!")
 
 
-    #def carAppr(self):
+    def carAppr(self):
+      if TL.carApr:
+        return True
+      else:
+        return False
 
-   # def
+    def Print(self):
+        return True
+
+    def change(self):
+      if TL.color == "green":
+        TL.color = "yellow"
+        time.sleep(3)
+      if TL.color == "yellow":
+        TL.color = "red"
+        time.sleep(8)
+      if TL.color == "red":
+        TL.color = "green"
+        time.sleep(8)
+        
+        
+    def runF(self):
+      while (TL.runn == True):       
+        if TL.carAppr() == True:
+          TL.color = "green"
+        TL.change()
+        
